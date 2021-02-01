2ba67c4b813cfb9c3bedde7096bc4f879b4d1af8
Blocksnmore <51055767+Blocksnmore@users.noreply.github.com>
2021-02-01 12:23:01 -0600

    More work on server + comments

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const { Socket } = require("socket.io");
+// Modify these settings if you have lots of games or players
+const settings = {
+  game: {
+    minpin: 1000, // The minumum value of the pin, Default: 1000
+    maxpin: 9999, // The maximum value of the pin, Default: 9999
+    maxplayers: 10, // Maximum amount of players in a game, Default: 10
+  },
+  player: {
+    minid: 1000, // The minimum value of a player's id, Default: 1000
+    maxid: 9999, // The maximum value of a player's id, Default: 9999
+  },
+};
 
+// Source
 var gamedata = new Map();
 var playerdata = new Map();
 const io = require("socket.io")();
@@ -7,28 +19,84 @@ io.listen(3000);
 
 io.on("connection", async (socket) => {
   var game = {
-    id: null, // Also used as the pin
-    type: null, // the type of game (name)
-    players: [],
+    host: {
+      type: null,
+      players: [],
+    },
+    socket: null,
+    id: null,
+    user: {
+      username: null,
+      userid: null,
+      gameid: null,
+    },
   };
-  socket.on("host", async (json) => {
+
+  // Main player joining stuff
+  socket.on("game.host", async (json) => {
+    // Creates a game session and saves the data
     game.id = generateGameID();
-    game.type = json.type;
+    game.host.type = json.type;
+    game.socket = socket;
     gamedata.set(game.id, game);
-    socket.emit("host", game);
+    socket.emit("game.createdgame", game);
+  });
+
+  socket.on("game.join", (json) => {
+    // Player joining mechanics
+    if (!gamedata.has(json.id)) return socket.emit("error.invalidid");
+    if (gamedata.get(json.id).host.players.length >= settings.game.maxplayers)
+      return socket.emit("error.full"); // May be removed if audience is added
+    game.user.gameid = json.id;
+    game.user.userid = generateUserID();
+    game.user.username = json.username;
+    playerdata.set(game.user.userid, game);
+    io.emit("game.playerjoin", game.user);
+    socket.emit("game.joined");
+  });
+
+  socket.on("disconnect", () => {
+    // Host/player disconnect system
+    // Host disconnect
+    if (game.id) {
+      io.emit("game.closed", { id: game.id });
+      gamedata.delete(game.id); // Make sure to delete the player id so that theres no issues with lots of games
+    }
+    // Player disconnect
+    if (game.user.id) {
+      io.emit("game.playerleave", {
+        id: game.user.id,
+        username: game.user.username,
+      });
+      playerdata.delete(game.user.id); // Make sure to delete the player id so that theres no issues with lots of players
+    }
+  });
+
+  socket.on("game.pause", (json) => {
+    // This is used as a "pause" menu. When the host is showing instructions or something
+    io.emit("game.pause", { id: json.id, img: json.img });
+  });
+
+  socket.on("game.unpause", (json) => {
+    // Disable the pause thing, althought i dont see really who would use it
+    io.emit("game.unpause", { id: json.id });
   });
 });
 
+// Generates an ID for the user
 function generateUserID() {
-  let id = getRandomInt(1, 9999);
+  let id = getRandomInt(settings.player.minid, settings.player.maxid);
   if (playerdata.has(id)) return generateUserID();
   else return id;
 }
+
+// Generates an ID for the game
 function generateGameID() {
-  let id = getRandomInt(1, 9999);
+  let id = getRandomInt(settings.game.minpin, settings.game.maxpin);
   if (gamedata.has(id)) return generateGameID();
   else return id;
 }
+// Random integer (doy)
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
