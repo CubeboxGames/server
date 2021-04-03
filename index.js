ba9942e6d785f955bf8c7c947305f1a0ed25b3c5
Blocksnmore <51055767+Blocksnmore@users.noreply.github.com>
2021-04-03 16:40:11 -0500

    Finish off code + security patch
    This patch adds security to the server, all events are now ran between the host and players connected to the host's game. It will not broadcast any data to other players connected

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const config = require("./config.json");
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
-const debug = true;
-var data = { hosts: new Map(), players: new Map() };
+const debug = config.debug;
+var data = { hosts: new Map() };
 
 const app = express();
 const server = http.createServer(app);
@@ -34,7 +34,8 @@ app.get("/", (req, res) => {
 
 io.on("connected", async (socket) => {
   if (debug) console.log("DEBUG: User connected to server");
-  socket.on("createGame", (maxplayers) => {
+
+  socket.on("createGame", (type, maxplayers) => {
     let code = generateGameCode();
     if (code === "MAX_GAMES") return socket.emit("gameError", "MAX_GAMES");
     data.hosts.set(code, {
@@ -42,14 +43,60 @@ io.on("connected", async (socket) => {
       socket: socket,
       max: maxplayers,
       id: socket.id,
+      type: type,
     });
-    socket.emit("createdGame", code);
+    if (debug) console.log("DEBUG: Game created!");
+    socket.emit("createdGame", code, socket.id);
   });
+
   socket.on("joinGame", (code, username) => {
     if (!data.hosts.has(code)) return socket.emit("gameError", "UNKNOWN_GAME");
     if (data.hosts.get(code).max >= data.hosts.get(code).players.length)
       return socket.emit("gameError", "FULL_GAME");
-      
+    if (debug) console.log("DEBUG: Player joined game!");
+    socket.emit("joinedGame", type);
+    data.hosts
+      .get(code)
+      .players.push({ username: username, code: code, socket: socket });
+    data.hosts.get(code).socket.emit("joinedGame", (username, socket.id));
+  });
+
+  socket.on("sendToPlayer", (gamecode, player, event, content) => {
+    if (!data.hosts.has(gamecode))
+      return socket.emit("gameError", "UNKNOWN_GAME");
+    let user = data.hosts
+      .get(gamecode)
+      .players.filter((p) => p.id === player)[0];
+    if (!user) return socket.emit("gameError", "UNKNOWN_USER");
+    user.socket.emit(event, content);
+  });
+
+  socket.on("sendToAllPlayers", (gamecode, event, content) => {
+    if (!data.hosts.has(gamecode))
+      return socket.emit("gameError", "UNKNOWN_GAME");
+    data.hosts.get(gamecode).players.forEach((p) => {
+      p.emit(event, content);
+    });
+  });
+  socket.on("sendToHost", (gamecode, event, content) => {
+    if (!data.hosts.get(gamecode).players.filter((p) => p.id === socket.id)[0])
+      return socket.emit("gameError", "INVALID_CALL");
+    data.hosts.get(gamecode).socket.emit(event, content);
+  });
+  socket.on("disconnect", () => {
+    for (let key in data.hosts.keys) {
+      let game = data.hosts.get(key);
+      if (game.id === socket.id) {
+        game.players.forEach((p) => {
+          p.socket.emit("gameClosed");
+        });
+        return data.hosts.delete(key);
+      }
+      if (game.players.filter((p) => p.id === socket.id)[0]) {
+        game.socket.emit("playerLeave", socket.id);
+        return (game.players = game.players.filter((p) => p.id !== socket.id));
+      }
+    }
   });
 });
 
